c9703d7151f05006bbe42e805d47370f399f4b35
tianhuzong <tianhuzong@github.com>
2024-07-17 18:30:46 +0800

    修改版本号

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("./README.md", mode='r', encoding='utf-8') as f:
 
 setup(
     name="thznumpy",      # 包名，用于安装和调用该包
-    version="0.0.2.dev20240716",               # 版本号
+    version="0.0.2.dev20240717",               # 版本号
     author="Sen",
     description="a math lib developed by Sen",
     long_description=des,
